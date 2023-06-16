68cc3a3054634b471a554d146bb38e6b22bc3801
crazypokerk <downnn@live.com>
2023-06-16 16:21:56 +0800

    实现了用wx.login获取用户头像和昵称并登录的效果

--- pages/confirm_login/user_login.js
+++ pages/confirm_login/user_login.js
@@ -5,40 +5,130 @@ Page({
    * 页面的初始数据
    */
   data: {
-    avatarUrl: '',
+    baseURL: "https://testforguncy.app.hzgcloud.cn/mini_program_test",
+    serverUrl: "https://testforguncy.app.hzgcloud.cn",
+    clientID: "f07a5a25-c9c6-409d-bb24-437185dc",
+    avatarUrl: '/resources/img/default_avatar.svg',
     nickname: ''
   },
 
-  onGetUserProfile: function () {
-    wx.login({
-      success: (res) => {
-        if (res.code) {
-          // 在这里调用后端API，使用res.code换取用户的openid和session_key
-          // 假设后端返回了用户信息
-          const userInfo = {
-            avatarUrl: 'https://example.com/avatar.jpg',
-            nickName: 'User Nickname'
-          };
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
+    this.setData({
+      avatarUrl,
+    })
+  },
 
-          this.setData({
-            avatarUrl: userInfo.avatarUrl,
-            nickname: userInfo.nickName
+  onGetUserProfile: function () {
+    let that = this;
+    wx.login({
+      timeout: 3000,
+      success: userResponse => {
+        if (userResponse.code) {
+          wx.showLoading({
+            title: "正在登录",
+            mask: true
           });
+          console.log(userResponse);
+          that.verifyLoginUser(userResponse.code, {
+            "nickname": that.data.nickname,
+            "avatarurl": that.data.avatarUrl,
+            "baseURL": that.data.baseURL
+          }, null);
         } else {
-          wx.showToast({
-            title: '登录失败',
-            contentText: err,
-            icon: 'none'
+          wx.showModal({
+            title: "错误",
+            confirmText: "确定"
           });
         }
-      },
-      fail: (err) => {
-        wx.showToast({
-          title: '登录失败',
-          contentText: err,
-          icon: 'none'
+      }
+    });
+    /**  
+    wx.getUserProfile({
+      desc: '需要获取用户信息用于登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (userResponse) => {
+        console.log(userResponse)
+        wx.showLoading({
+          title: "正在登录",
+          mask: true
+        });
+        wx.login({
+          success: res => {
+            if (res.code) {
+              that.verifyLoginUser(res.code, userResponse, null);
+            }
+          },
+          fail: error => {
+            wx.hideLoading();
+            wx.showModal({
+              title: "错误",
+              content: error,
+              confirmText: "确定"
+            });
+          }
         });
       }
+    })
+    */
+  },
+  verifyLoginUser: function (code, userObj, openid) {
+    let that = this;
+    console.log("userObj");
+    console.log(userObj);
+    wx.request({
+      url: `${that.data.baseURL}/ServerCommand/GetRequestToken`,
+      method: "POST",
+      data: JSON.stringify({
+        "client_id": that.data.clientID,
+        "serverURL": that.data.serverUrl
+      }),
+      success(response) {
+        console.log(response);
+        let url = null, body = null;
+        if (userObj == null) {
+          url = `${that.data.baseURL}/ServerCommand/GetSSOToken`;
+          body = {
+            "userName": openid,
+            "baseURL": that.data.baseURL
+          }
+        }
+        if (openid == null) {
+          url = `${that.data.baseURL}/ServerCommand/AddUserAndLogin`;
+          body = {
+            "code": code,
+            "fullname": userObj.nickname,
+            "avatarurl": userObj.avatarurl,
+            "baseURL": userObj.baseURL
+          }
+        }
+        wx.request({
+          url: url,
+          method: "POST",
+          header: { "Authorization": response.data.Authorization },
+          data: JSON.stringify(body),
+          success(response) {
+            console.log(response);
+            wx.setStorage({
+              key: "openid",
+              data: response.data.openid,
+              success: () => {
+                if (response.data.ErrCode === 0) {
+                  console.log(response.data.redirectURL);
+                  wx.navigateTo({
+                    url: `/pages/webview/forguncy_webview?url=${encodeURIComponent(response.data.redirectURL)}`
+                  })
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  },
+
+  onInputChange: function (e) {
+    this.setData({
+      nickname: e.detail.value
     });
   },
 
@@ -46,7 +136,76 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    let that = this;
+    wx.login({
+      success(result) {
+        if (result.code) {
+          console.log(result.code);
+          console.log(that.data.baseURL);
+          wx.request({
+            url: `${that.data.baseURL}/ServerCommand/GetRequestToken`,
+            method: "POST",
+            data: JSON.stringify({
+              "client_id": that.data.clientID,
+              "serverURL": that.data.serverUrl
+            }),
+            fail: err => {
+              console.log(`/ServerCommand/GetRequestToken${err}`);
+              wx.hideLoading();
+            },
+            success: response => {
+              console.log("response.data:::");
+              console.log(response.data);
+              wx.request({
+                url: `${that.data.baseURL}/ServerCommand/Code2SessionID`,
+                method: "POST",
+                header: { "Authorization": response.data.Authorization },
+                data: { "code": result.code },
+                fail: err => {
+                  console.log(`/ServerCommand/Code2SessionID${err}`);
+                  wx.hideLoading();
+                },
+                success: code2SessionResult => {
+                  console.log("code2SessionResult:::");
+                  console.log(code2SessionResult.data);
+                  if (code2SessionResult.data.openid) {
+                    wx.request({
+                      url: `${that.data.baseURL}/ServerCommand/GetOpenidCount`,
+                      method: "POST",
+                      header: { "Authorization": response.data.Authorization },
+                      data: { "openid": code2SessionResult.data.openid },
+                      fail: err => {
+                        console.log(`/ServerCommand/GetOpenidCount${err}`);
+                        wx.hideLoading();
+                      },
+                      success: countResult => {
+                        console.log("countResult:::");
+                        console.log(countResult);
+                        if (countResult.data.Message > 0) {
+                          wx.showLoading({
+                            title: "自动登录中",
+                            mask: true
+                          })
+                          that.verifyLoginUser(result.code, null, code2SessionResult.data.openid);
+                        } else {
+                          wx.hideLoading();
+                          console.log("end --> wx.login")
+                        }
+                      }
+                    })
+                  } else {
+                    wx.showModal({
+                      content: code2SessionResult.data.Message,
+                      confirmText: "确定"
+                    });
+                  }
+                }
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,9 +6,6 @@ var markersData = [];
 
 Page({
   data: {
-    baseURL: "https://testforguncy.app.hzgcloud.cn/mini_program_test",
-    serverUrl: "https://testforguncy.app.hzgcloud.cn",
-    clientID: "f07a5a25-c9c6-409d-bb24-437185dc",
     makers: [],
     latitude: '',
     longitude: '',
@@ -28,135 +25,8 @@ Page({
       mask: true
     });
 
-    // 初始化登录
-    wx.login({
-      success(result) {
-        if (result.code) {
-          console.log(result.code);
-          console.log(that.data.baseURL);
-          wx.request({
-            url: `${that.data.baseURL}/ServerCommand/GetRequestToken`,
-            method: "POST",
-            data: JSON.stringify({
-              "client_id": that.data.clientID,
-              "serverURL": that.data.serverUrl
-            }),
-            fail: err => {
-              console.log(`/ServerCommand/GetRequestToken${err}`);
-              wx.hideLoading();
-            },
-            success: response => {
-              console.log("response.data:::");
-              console.log(response.data);
-              wx.request({
-                url: `${that.data.baseURL}/ServerCommand/Code2SessionID`,
-                method: "POST",
-                header: { "Authorization": response.data.Authorization },
-                data: { "code": result.code },
-                fail: err => {
-                  console.log(`/ServerCommand/Code2SessionID${err}`);
-                  wx.hideLoading();
-                },
-                success: code2SessionResult => {
-                  console.log("code2SessionResult:::");
-                  console.log(code2SessionResult.data);
-                  if (code2SessionResult.data.openid) {
-                    wx.request({
-                      url: `${that.data.baseURL}/ServerCommand/GetOpenidCount`,
-                      method: "POST",
-                      header: { "Authorization": response.data.Authorization },
-                      data: { "openid": code2SessionResult.data.openid },
-                      fail: err => {
-                        console.log(`/ServerCommand/GetOpenidCount${err}`);
-                        wx.hideLoading();
-                      },
-                      success: countResult => {
-                        console.log("countResult:::");
-                        console.log(countResult);
-                        if (countResult.data.Message > 0) {
-                          wx.showLoading({
-                            title: "自动登录中",
-                            mask: true
-                          })
-                          that.verifyLoginUser(result.code, null, code2SessionResult.data.openid);
-                        } else {
-                          wx.hideLoading();
-                          console.log("end --> wx.login")
-                        }
-                      }
-                    })
-                  } else {
-                    wx.showModal({
-                      content: code2SessionResult.data.Message,
-                      confirmText: "确定"
-                    });
-                  }
-                }
-              })
-            }
-          })
-        }
-
-      }
-    })
-  },
-  verifyLoginUser: function (code, userResponse, openid) {
-    let that = this;
-    console.log("userResponse");
-    console.log(userResponse);
-    wx.request({
-      url: `${that.data.baseURL}/ServerCommand/GetRequestToken`,
-      method: "POST",
-      data: JSON.stringify({
-        "client_id": that.data.clientID,
-        "serverURL": that.data.serverUrl
-      }),
-      success(response) {
-        console.log(response);
-        let url = null, body = null;
-        if (userResponse == null) {
-          url = `${that.data.baseURL}/ServerCommand/GetSSOToken`;
-          body = {
-            "userName": openid,
-            "baseURL": that.data.baseURL
-          }
-        }
-        if (openid == null) {
-          url = `${that.data.baseURL}/ServerCommand/AddUserAndLogin`;
-          body = {
-            "code": code,
-            "fullname": userResponse.userInfo.nickName,
-            "avatarurl": userResponse.userInfo.avatarUrl,
-            "gender": userResponse.userInfo.gender,
-            "country": userResponse.userInfo.country,
-            "province": userResponse.userInfo.province,
-            "city": userResponse.userInfo.city,
-            "language": userResponse.userInfo.language,
-            "baseURL": that.data.baseURL
-          }
-        }
-        wx.request({
-          url: url,
-          method: "POST",
-          header: { "Authorization": response.data.Authorization },
-          data: body,
-          success(response) {
-            console.log(response);
-            wx.setStorage({
-              key: "openid",
-              data: response.data.openid,
-              success: () => {
-                if (response.data.ErrCode === 0) {
-                  console.log(response.data.redirectURL);
-                  wx.navigateTo({
-                    url: `/pages/webview/forguncy_webview?url=${encodeURIComponent(response.data.redirectURL)}`
-                  })
-                }
-              }
-            })
-          }
-        })
-      }
+    wx.navigateTo({
+      url: "/pages/confirm_login/user_login"
     })
   },
   markertap: function (e) {
@@ -232,32 +102,7 @@ Page({
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    let that = this;
-    wx.getUserProfile({
-      desc: '需要获取用户信息用于登录', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (userResponse) => {
-        console.log(userResponse)
-        wx.showLoading({
-          title: "正在登录",
-          mask: true
-        });
-        wx.login({
-          success: res => {
-            if (res.code) {
-              that.verifyLoginUser(res.code, userResponse, null);
-            }
-          },
-          fail: error => {
-            wx.hideLoading();
-            wx.showModal({
-              title: "错误",
-              content: error,
-              confirmText: "确定"
-            });
-          }
-        });
-      }
-    })
+
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
